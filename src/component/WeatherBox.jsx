d0b8ef5bd058109180052ae9272c7d598daa0816
Honsungyeon <hosuye@gamil.com>
2025-10-25 00:27:19 +0900

    화씨 소수점1자리로 수정

--- src/component/WeatherBox.jsx
+++ src/component/WeatherBox.jsx
@@ -6,8 +6,7 @@ const WeatherBox = ({weather}) => {
   return (
     <div className="weather-box">
       <h1>{weather?.name}</h1>
-      <h2>섭씨{weather?.main.temp}℃/ 화씨{((weather?.main.temp)*9/5)+32
-} °F</h2>
+      <h2>섭씨{weather?.main.temp}℃/ 화씨{(((weather?.main.temp)*9/5)+32).toFixed(1)} °F</h2>
       <h3>{weather?.weather[0].description}</h3>
         <h2>최고온도 {weather?.main.temp_max} /최저온도 {weather?.main.temp_min}</h2>
     </div>
